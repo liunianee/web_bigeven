3cfa5fb78f8f52a8f392d4f975730f2c4296fda4
liunian <418362221@qq.com>
2020-09-20 15:18:09 +0800

    完成了登录和注册功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,7 @@
+//再发起请求前会先调用这个函数，options可以拿到ajax的配置对象
+
 $.ajaxPrefilter(function(options) {
-    console.log(options.url);
+    // console.log(options.url);
+    //在发起ajax请求之前统一拼接请求根路经
     options.url = 'http://ajax.frontend.itheima.net' + options.url
 })
